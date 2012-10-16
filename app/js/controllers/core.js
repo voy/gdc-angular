5d08a0e6887c7434e1fe2c12b8bc8f7d236136f9
Roman Krejcik <roman.krejcik@gooddata.com>
2012-10-16 13:15:13 +0200

    login/logout cause header refresh

--- app/js/controllers/core.js
+++ app/js/controllers/core.js
@@ -8,24 +8,37 @@ var RedirectToProjectCntl = function($scope, $location, bootstrap) {
 };
 
 
-var ProfileCntl = function($scope, $location, bootstrap, logout) {
+var ProfileCntl = function($scope, $rootScope, $location, bootstrap, logout) {
     $scope.authenticated = false;
 
-    bootstrap().done(function(res) {
-        var account = res.accountSetting;
-        $scope.name = account.firstName + ' ' + account.lastName;
-        $scope.login = account.login;
-        $scope.authenticated = true;
-    }.withScope($scope));
+    function refreshUser() {
+        bootstrap().done(function(res) {
+            var account = res.accountSetting;
+            $scope.name = account.firstName + ' ' + account.lastName;
+            $scope.login = account.login;
+            $scope.authenticated = true;
+        }.withScope($scope));
+    }
+
+    refreshUser();
 
     $scope.logout = function() {
         logout().done(function() {
             $scope.authenticated = false;
             delete $scope.name;
             delete $scope.login;
+            $rootScope.$broadcast('logoutSuccess');
+            $location.path('/login');
+        }.withScope($scope)).fail(function() {
+            //e.g. already logged out
             $location.path('/login');
         }.withScope($scope));
     };
+
+    //Profile controller is outsidr ng-view, so nobody refresh user data automatically
+    $scope.$on('loginSuccess', function() {
+        refreshUser();
+    });
 };
 
 var ProjectsCntl = function($scope, $rootScope, $location, $routeParams, projects) {
@@ -41,22 +54,33 @@ var ProjectsCntl = function($scope, $rootScope, $location, $routeParams, project
         }
     }
 
-    projects().done(function(res) {
-        res = res.map(function(p) {
-            return {
-                id: Utils.url2id(p.links.self),
-                url: p.links.self,
-                title: p.meta.title
-            };
-        });
+    function refreshProjects() {
+        projects().done(function(res) {
+            res = res.map(function(p) {
+                return {
+                    id: Utils.url2id(p.links.self),
+                    url: p.links.self,
+                    title: p.meta.title
+                };
+            });
 
-        $scope.projects = res;
-        refreshCurrentProject();
-    }.withScope($scope));
+            $scope.projects = res;
+            refreshCurrentProject();
+        }.withScope($scope));
+    }
+
+    refreshProjects();
 
     $rootScope.$on("$routeChangeSuccess", function(event, next, current) {
         //controller is outside ngView and it is not recreated on route change
         refreshCurrentProject();
+    });
+
+    $scope.$on('loginSuccess', function() {
+        refreshProjects();
+    });
+    $scope.$on('logoutSuccess', function() {
+        $scope.projects = [];
     });
 
     $scope.switchProject = function() {
@@ -64,11 +88,13 @@ var ProjectsCntl = function($scope, $rootScope, $location, $routeParams, project
     };
 };
 
-var LoginCntl = function($scope, $location, login) {
+var LoginCntl = function($scope, $rootScope, $location, login) {
     $scope.username = 'bear@gooddata.com';
+
     $scope.login = function() {
         delete $scope.message;
         login($scope.username, $scope.password).done(function() {
+            $rootScope.$broadcast('loginSuccess');
             $location.path('/');
         }.withScope($scope)).fail(function() {
             $scope.message = '-------------Invalid-------------';
